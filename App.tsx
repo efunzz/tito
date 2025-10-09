4fb2efb1eea1b9edf6f3280ce67b302fb6602c26
efunzz <irfansofyan2001@gmail.com>
2025-10-10 00:13:56 +0800

    Initial TITO setup - TypeScript project with navigation

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Welcome to TITO</Text>
+      <Text style={styles.subTitle}>Time in Time out</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  subTitle: {
+    fontSize: 20,
+  }
 });
